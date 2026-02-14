6726e4d8bdad9b3961de99a1afa22e4bb1c7868f
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2026-02-14 22:42:55 +0530

    fix ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
+import nextTypescript from "eslint-config-next/typescript.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default defineConfig([{
-    extends: [...nextCoreWebVitals, ...nextTypescript],
-}]);
+export default [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+];
